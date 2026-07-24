d029abc1351a90092f32274d220d54c15d0351da
dbantje <david.bantje@pik-potsdam.de>
2026-07-24 11:03:38 +0200

    Full .mif processing for premise

--- scripts/input/LCA_workflows.py
+++ scripts/input/LCA_workflows.py
@@ -9,6 +9,7 @@ import xarray as xr
 from internalizer import Internalizer
 from internalizer.internalizer import CONFIG_NO_REMOVAL
 from internalizer.utils import get_automatic_exclude_list
+from internalizer.mif_processing import process_mif
 
 EI_VERSION = "3.10.1"
 YEARS_INTERNALIZATION = [2020, 2030, 2040, 2050, 2060, 2070]
@@ -65,47 +66,6 @@ def get_impact_categories(args):
 
     return ics
         
-def add_ES_subcategories(mifpath):
-    """
-    Add subcategories for ES transport variables that are needed for premise.
-    """
-    df = pd.read_csv(mifpath, sep=";").iloc[:, :-1]
-
-    all_variables = list(df["Variable"].unique())
-    ES_variables = [v for v in all_variables if v.startswith("ES|Transport")]
-    parent_variables = [v for v in ES_variables if v.split("|")[-1] in ["Liquids", "Gases"]]
-    dflist = []
-    for parent in parent_variables:
-        prefix = parent.replace("ES|", "FE|")
-        children = [v for v in all_variables if v.startswith(prefix + "|")]
-        if len(children) == 3:
-            subcats = [v.split("|")[-1] for v in children]
-            subcats2add = [cat for cat in subcats if "|".join([parent, cat]) not in all_variables]
-
-            if len(subcats2add) > 0:
-                years = list(df.columns)[5:]
-
-                total = df[df["Variable"] == prefix].set_index("Region")[years].astype(float)
-                parent_df = df[df["Variable"] == parent]
-                new_unit = list(parent_df["Unit"].unique())[0]
-                parent_df = parent_df.set_index("Region")[years].astype(float)
-
-                for cat in subcats2add:
-                    s = prefix + "|" + cat
-                    sel = df[df["Variable"] == s].copy().set_index("Region")[years].astype(float)
-                    share = sel.div(total, axis=0)
-                    new_df = share.mul(parent_df, axis=0).reset_index()
-                    new_df["Model"] = list(df["Model"].unique())[0]
-                    new_df["Scenario"] = list(df["Scenario"].unique())[0]
-                    new_df["Unit"] = new_unit
-                    new_df["Variable"] = "|".join([parent, cat])
-                    dflist.append(new_df)
-
-    if len(dflist) > 0:
-        additions = pd.concat(dflist, axis=0)[["Model", "Scenario", "Region", "Variable", "Unit"]+years]
-        pd.concat((df, additions), axis=0).to_csv(mifpath, sep=";", index=False)
-
-
 
 if __name__ == "__main__":
     # Parsing arguments
@@ -146,7 +106,9 @@ if __name__ == "__main__":
         ]
     )
     logFile.writelines(["\n"])
-        
+
+    # append to reporting
+    process_mif(args.mifpath, model="remind")   
 
     # in any case, initialize an Internalizer instance and call the setup
     bw_project = f"scenarioLCA_{EI_VERSION}"
@@ -163,7 +125,6 @@ if __name__ == "__main__":
 
     if args.plca:
         t0 = time.time()
-        add_ES_subcategories(args.mifpath)
         I.run_premise(YEARS_INTERNALIZATION, include_interventions=not args.no_interventions)
         t1 = time.time()
         logFile.writelines([f"Premise runs done in {t1-t0} seconds", "\n"])
--- scripts/utils/fresh_premise_setup.py
+++ scripts/utils/fresh_premise_setup.py
@@ -5,7 +5,7 @@ EI_VERSION = "3.10.1"
 # set paths a previous run
 folder = "/p/tmp/davidba/internalization_develop/remind/output/SSP2-only-nonCC-ep-v0p8p3_2026-07-22_12.11.59"
 pathway = "SSP2-only-nonCC-ep-v0p8p3"
-mifpath = folder + f"/lca/remind_runs/{pathway}.mif"
+mifpath = folder + f"/lca/remind_runs/remind_{pathway}.mif"
 gdxpath = folder + "/fulldata.gdx"
 
 # in any case, initialize an Internalizer instance and call the setup
